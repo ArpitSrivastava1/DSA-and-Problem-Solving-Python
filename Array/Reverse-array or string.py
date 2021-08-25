b3d98f4c78c4179558d55b75f1d92cd5323e4491
Arpit Srivastava <79218928+ArpitSrivastava1@users.noreply.github.com>
2021-08-25 22:41:54 +0530

    Create Reverse-array or string.py
    Problem Link: https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/

---
+++ Array/Reverse-array or string.py
@@ -0,0 +1,14 @@
+def reverseWord(s):
+    return s[::-1]
+    
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while(t>0):
+        s = input()
+        print(reverseWord(s))
+        t = t-1
